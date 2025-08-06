ade5f1ef2d374ff1686811ee64ae86638194c82a
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2025-08-06 15:26:39 +0530

    Added Xcode project and made initial comment

---
+++ FirstAppSwiftUI/FirstAppSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstAppSwiftUI
+//
+//  Created by G Yacobu on 06/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .font(.title)
+                .fontWeight(.black)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstAppSwiftUI/FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwiftUIApp.swift
+//  FirstAppSwiftUI
+//
+//  Created by G Yacobu on 06/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
